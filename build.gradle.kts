9d2dff3602f285da708fabcc27747685c3da9154
MrXiaoM <coolxiaom95@gmail.com>
2025-08-31 16:57:42 +0800

    use copy task instead of set classifier

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,14 +24,14 @@ dependencies {
 
     compileOnly("net.milkbowl.vault:VaultAPI:1.7")
     compileOnly("me.clip:placeholderapi:2.11.6")
+    compileOnly("org.jetbrains:annotations:24.0.0")
 
     implementation("net.kyori:adventure-api:4.22.0")
     implementation("net.kyori:adventure-platform-bukkit:4.4.0")
     implementation("net.kyori:adventure-text-minimessage:4.22.0")
     implementation("de.tr7zw:item-nbt-api:2.15.2-SNAPSHOT")
-    implementation("com.github.technicallycoded:FoliaLib:0.4.4")
-    implementation("org.jetbrains:annotations:24.0.0")
     implementation("top.mrxiaom:PluginBase:1.5.4")
+    implementation("com.github.technicallycoded:FoliaLib:0.4.4") { isTransitive = false }
     for (nms in project.project(":nms").subprojects) {
         if (nms.name == "shared") implementation(nms)
         if (nms.name.startsWith("v")) add("shadowLink", nms)
@@ -45,11 +45,8 @@ java {
 }
 tasks {
     shadowJar {
-        archiveClassifier.set("")
         configurations.add(project.configurations.getByName("shadowLink"))
         mapOf(
-            "org.intellij.lang.annotations" to "annotations.intellij",
-            "org.jetbrains.annotations" to "annotations.jetbrains",
             "top.mrxiaom.pluginbase" to "base",
             "de.tr7zw.changeme.nbtapi" to "nbtapi",
             "net.kyori" to "kyori",
@@ -58,8 +55,14 @@ tasks {
             relocate(original, "$shadowGroup.$target")
         }
     }
-    build {
+    val copyTask = create<Copy>("copyBuildArtifact") {
         dependsOn(shadowJar)
+        from(shadowJar.get().outputs)
+        rename { "${project.name}-$version.jar" }
+        into(rootProject.file("out"))
+    }
+    build {
+        dependsOn(copyTask)
     }
     withType<JavaCompile>().configureEach {
         options.encoding = "UTF-8"
