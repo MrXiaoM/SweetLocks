e809cf6dd965fa93a9f765670593f0cbf0a20f37
MrXiaoM <coolxiaom95@gmail.com>
2025-10-05 10:16:33 +0800

    add test command to check block meta

--- src/main/java/top/mrxiaom/sweet/locks/commands/CommandMain.java
+++ src/main/java/top/mrxiaom/sweet/locks/commands/CommandMain.java
@@ -1,11 +1,15 @@
 package top.mrxiaom.sweet.locks.commands;
         
 import com.google.common.collect.Lists;
+import de.tr7zw.changeme.nbtapi.NBT;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.configuration.MemoryConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.util.RayTraceResult;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import top.mrxiaom.pluginbase.func.AutoRegister;
@@ -33,6 +37,22 @@ public class CommandMain extends AbstractModule implements CommandExecutor, TabC
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (args.length == 1 && "meta".equalsIgnoreCase(args[0]) && sender.isOp() && sender instanceof Player) {
+            Player player = (Player) sender;
+            RayTraceResult result = player.rayTraceBlocks(5);
+            Block block = result == null ? null : result.getHitBlock();
+            if (block == null) {
+                return t(player, "你的准心未指向一个方块");
+            }
+            t(player, "==========================");
+            t(player, "方块类型: " + block.getType().name());
+            NBT.getPersistentData(block.getState(), nbt -> {
+                t(player, nbt.toString());
+                return null;
+            });
+            t(player, "==========================");
+            return true;
+        }
         if (args.length == 1 && "reload".equalsIgnoreCase(args[0]) && sender.isOp()) {
             plugin.reloadConfig();
             return Messages.commands__reload.tm(sender);
